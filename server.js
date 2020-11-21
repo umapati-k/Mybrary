0bf8e596700d21c970ce894711ec0955f73fcd13
umapati <umzaz.88@gmail.com>
2020-11-21 20:57:08 +0530

    finished author routes

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ if (process.env.NODE_ENV !== 'production') {
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
   const bodyParser = require('body-parser')
-  
+  const methodOverride=require('method-override')
+
   const indexRouter = require('./routes/index')
   const authorRouter = require('./routes/authors')
   const bookRouter = require('./routes/books')
@@ -17,7 +18,8 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(expressLayouts)
   app.use(express.static('public'))
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-  
+  app.use(methodOverride('_method'))
+
   const mongoose = require('mongoose')
   mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true,
 useUnifiedTopology: true })
