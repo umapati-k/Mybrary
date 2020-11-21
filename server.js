fa7357a48ae7b4477feb2d9110b868a3ab9531da
umapati <umzaz.88@gmail.com>
2020-11-21 12:07:14 +0530

    initial author routes

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ if(process.env.NODE_ENV !=='production'){
 
 const express=require("express");
 const app=express();
+const bodyParser=require('body-parser')
+
 const expressLayouts=require("express-ejs-layouts")
+
 const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/authors')
+
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+
 const mongoose=require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
 	useNewUrlParser:true,
@@ -19,7 +26,9 @@ mongoose.connect(process.env.DATABASE_URL,{
 const db=mongoose.connection
 db.on('error',error=> console.error(error))
 db.once('open',()=> console.log('connected to mongoose'))
+
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 
 
